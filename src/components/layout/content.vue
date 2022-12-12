7a081a0be6bafe04b0f7d605f0f142de71ba94d8
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-12 21:42:03 +0500

    features section added

--- src/components/layout/content.vue
+++ src/components/layout/content.vue
@@ -21,18 +21,37 @@
         </div>
       </div>
     </section>
+    <section class="content__features">
+      <div class="content__header">
+        <p class="content__header--title">Features</p>
+        <p class="content__header--description">
+          Our aim is to make it quick and easy for you to access your <br />
+          favourite websites.Your Bookmarks sync between your devices <br />
+          so you can access them on the go
+        </p>
+        <ul class="content__features--tabs">
+          <li class="active">Simple Bookmarking</li>
+          <li>Speedy Searching</li>
+          <li>Easy Sharing</li>
+        </ul>
+      </div>
+      <component :is="activeTab"></component>
+    </section>
   </main>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import HeroIcon from "../icons/heroIcon.vue";
+import BookMark from "./reusable/bookmark.vue";
 
 export default defineComponent({
   components: {
     HeroIcon,
+    BookMark,
   },
   setup() {
-    return {};
+    const activeTab = ref(BookMark);
+    return { activeTab };
   },
 });
 </script>
---
+++ src/components/layout/reusable/bookmark.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content__features--bookmarkTab">
+    <div class="bookmarkTab__image">
+      <div class="image__wrapper">
+        <!-- <tabOneIcon class="image" /> -->
+        <img
+          class="image"
+          src="../../../assets/images/illustration-features-tab-1.svg"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="bookmarkTab__details">
+      <h2 class="bookmarkTab__details--title">Bookmark in one click</h2>
+      <p class="bookmarkTab__details--description">
+        Organize your bookmarks however you like.Our <br />simple drag-n-drop
+        inteface gives you complete <br />
+        control over how you manage your favourite sites.
+      </p>
+      <button class="btn">More Info</button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import tabOneIcon from "@/components/icons/tab-one.vue";
+
+export default defineComponent({
+  components: { tabOneIcon },
+  setup() {
+    return {};
+  },
+});
+</script>
