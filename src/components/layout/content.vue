bec334f663aad9c81bb37268cf06bb196ce4f018
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-12 12:03:51 +0500

    content section is added

---
+++ src/components/layout/content.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="content">
+    <section class="content__intro">
+      <p class="content__intro--title">
+        A Simple Bookmark <span> Manager </span>
+      </p>
+      <p class="content__intro--description">
+        A clean and simple interface to organize your favourite <br />
+        websites.Open a new browser tab and see your sites load <br />
+        intantly.Try it for free.
+      </p>
+      <div class="content__intro--buttons">
+        <button class="btn">Get it on Chrome</button>
+        <button class="btn">Get it on Firefox</button>
+      </div>
+    </section>
+  </main>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,13 +1,16 @@
 <template>
   <headerVue />
+  <contentVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import headerVue from "./header.vue";
+import contentVue from "./content.vue";
 
 export default defineComponent({
   components: {
     headerVue,
+    contentVue,
   },
   setup() {
     return {};
