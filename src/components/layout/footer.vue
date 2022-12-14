e8e2d723f6e04ccc444b4df29b754076758a7b56
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-14 23:14:20 +0500

    footer added

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <ul class="footer__list">
+      <li><logoIconVue /></li>
+      <li>Features</li>
+      <li>Pricing</li>
+      <li>Contact</li>
+    </ul>
+    <div class="footer__icons">
+      <iconFacebookVue />
+      <iconTwitterVue />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import logoIconVue from "../icons/logoIcon.vue";
+import iconFacebookVue from "../icons/icon-facebook.vue";
+import iconTwitterVue from "../icons/icon-twitter.vue";
+
+export default defineComponent({
+  components: { logoIconVue, iconFacebookVue, iconTwitterVue },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/layout/index.vue
+++ src/components/layout/index.vue
@@ -1,16 +1,19 @@
 <template>
   <headerVue />
   <contentVue />
+  <footerVue />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import headerVue from "./header.vue";
 import contentVue from "./content.vue";
+import footerVue from "./footer.vue";
 
 export default defineComponent({
   components: {
     headerVue,
     contentVue,
+    footerVue,
   },
   setup() {
     return {};
