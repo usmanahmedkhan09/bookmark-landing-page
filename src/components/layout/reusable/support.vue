a4a50b98bf513018282dbdc43d18d369c1c6dc99
usman ahmed khan <usmanahmedkhan@live.com>
2022-12-16 21:11:01 +0500

    email validation added

--- src/components/layout/reusable/support.vue
+++ src/components/layout/reusable/support.vue
@@ -5,16 +5,65 @@
     we're doing
   </p>
   <div class="content__support--contact">
-    <input type="text" placeholder="Enter your email address" />
-    <button class="btn">Contact Us</button>
+    <div class="input__wrapper" :class="error.state ? 'error' : ''">
+      <input
+        v-model="email"
+        type="text"
+        placeholder="Enter your email address"
+        @keypress="resetState()"
+      />
+      <span v-if="error.state"> {{ error.message }}</span>
+      <img
+        v-if="error.state"
+        class=""
+        src="../../../assets/images/icon-error.svg"
+        alt=""
+      />
+    </div>
+    <button class="btn" @click="validateEmail()">Contact Us</button>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return {};
+    const email = ref("");
+
+    let error = ref({
+      message: "",
+      state: false,
+    });
+
+    const validateEmail = () => {
+      if (email.value.length > 0) {
+        if (
+          String(email.value)
+            .toLowerCase()
+            .match(
+              /^([A-Za-z0-9_\-\.])+\@(?!(?:[A-Za-z0-9_\-\.]+\.)?([A-Za-z]{2,4})\.\2)([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+            )
+          // .match(
+          //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          // )
+        ) {
+          return "";
+        } else {
+          error.value.message = "Whoops, make sure it's an email";
+          error.value.state = true;
+        }
+      } else {
+        error.value.message = "Email is required.";
+        error.value.state = true;
+      }
+    };
+
+    const resetState = () => {
+      error.value.state = false;
+      error.value.message = "";
+    };
+
+    return { email, validateEmail, error, resetState };
   },
 });
 </script>
